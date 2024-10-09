453f6730d6f8a4ba4e319690303c521e2e9b8255
KhanhDinh <22025516@vnu.edu.vn>
2024-10-09 16:49:29 +0700

    Slide and hero update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,10 @@
+"use client";
 import React from "react";
+import Slider from "react-slick";
+import Slide from "./Slide";
 
 const Hero = () => {
-  let settings = {
+  const settings = {
     dots: true,
     infinite: true,
     slidesToShow: 1,
@@ -13,26 +16,42 @@ const Hero = () => {
   const slideData = [
     {
       id: 0,
-      img: "",
+      img: "/banner1.jpg",
       title: "Trending Item",
       mainTitle: "WOMEN'S LATEST FASHION SALE",
       price: "$20",
     },
     {
       id: 1,
-      img: "",
+      img: "/banner2.jpg",
       title: "Trending Accessories",
       mainTitle: "MODERN SUNGLASSES",
       price: "$15",
     },
     {
       id: 2,
-      img: "",
+      img: "/banner1.jpg",
       title: "Sale Offer",
       mainTitle: "NEW FASHION SUMMER SALE",
       price: "$30",
     },
   ];
-  return <div>Hero</div>;
+  return (
+    <div>
+      <div className="container pt-6 lg:pt-0">
+        <Slider {...settings}>
+          {slideData.map((item) => (
+            <Slide
+              key={item.id}
+              img={item.img}
+              title={item.title}
+              mainTitle={item.mainTitle}
+              price={item.price}
+            />
+          ))}{" "}
+        </Slider>
+      </div>
+    </div>
+  );
 };
 export default Hero;
